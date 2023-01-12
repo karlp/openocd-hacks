karlp/openocd-hacks

commit a82b7041570ed1495b48f2cca8706197cbc160cb

Import MounRiver WCH fork
This corresponds to binary openocd provided in MRS toolcahin linux v1.40

Open On-Chip Debugger 0.11.0+dev-02215-gcc0ecfb6d-dirty (2022-03-30-18:53)

Requested via email to support@mounriver.com

"Careful analysis" lead me to find that the fork point for this dump was
cc0ecfb6d5b9 (that commit id is actually reported by the MRS binary, but
it doesn't exist in openocd upstream, so I manually bisected looking at
code changes, before the duh moment of realizing this was forked from
the riscv fork, not from upstream :)

I've cleaned up a bunch of file mode changes to make the patchset
not look so insane.

I've left all the whitespace changes as received though.

src/helper/options.c
@@ -46,7 +46,7 @@
 #endif
 
 static int help_flag, version_flag;
-
+extern int noloadflag;
 static const struct option long_options[] = {
 	{"help",		no_argument,			&help_flag,		1},
 	{"version",		no_argument,			&version_flag,	1},
@@ -263,7 +263,8 @@ static void add_default_dirs(void)
 		free(path);
 	}
 
-	path = alloc_printf("%s/%s/%s", exepath, bin2data, "scripts");
+	//path = alloc_printf("%s/%s/%s", exepath, bin2data, "scripts");
+	path = alloc_printf("%s/%s/%s", exepath, "../", "scripts");
 	if (path) {
 		add_script_search_dir(path);
 		free(path);
@@ -277,11 +278,12 @@ int parse_cmdline_args(struct command_context *cmd_ctx, int argc, char *argv[])
 {
 	int c;
 
+
 	while (1) {
 		/* getopt_long stores the option index here. */
 		int option_index = 0;
 
-		c = getopt_long(argc, argv, "hvd::l:f:s:c:", long_options, &option_index);
+		c = getopt_long(argc, argv, "hvd::l:f:s:c:x", long_options, &option_index);
 
 		/* Detect the end of the options. */
 		if (c == -1)
@@ -318,8 +320,11 @@ int parse_cmdline_args(struct command_context *cmd_ctx, int argc, char *argv[])
 					command_run_linef(cmd_ctx, "log_output %s", optarg);
 				break;
 			case 'c':		/* --command | -c */
-				if (optarg)
-				    add_config_command(optarg);
+				if (optarg)				
+				    add_config_command(optarg);								
+				break;
+			case 'x':              // for compatible	
+				noloadflag=1;
 				break;
 			default:  /* '?' */
 				/* getopt will emit an error message, all we have to do is bail. */

configure.ac
@@ -112,6 +112,7 @@ m4_define([USB1_ADAPTERS],
 	[[[ftdi], [MPSSE mode of FTDI based devices], [FTDI]],
 	[[ftdi_oscan1], [cJTAG OSCAN1 tunneled thru MPSSE], [FTDI_OSCAN1]],
 	[[stlink], [ST-Link Programmer], [HLADAPTER_STLINK]],
+  [[wlink], [WLink Programmer], [WLINK]],
 	[[ti_icdi], [TI ICDI JTAG Programmer], [HLADAPTER_ICDI]],
 	[[ulink], [Keil ULINK JTAG Programmer], [ULINK]],
 	[[usb_blaster_2], [Altera USB-Blaster II Compatible], [USB_BLASTER_2]],
@@ -274,6 +275,10 @@ AC_ARG_ENABLE([parport_giveio],
 AC_ARG_ENABLE([jtag_vpi],
   AS_HELP_STRING([--enable-jtag_vpi], [Enable building support for JTAG VPI]),
   [build_jtag_vpi=$enableval], [build_jtag_vpi=no])
+
+AC_ARG_ENABLE([wlink],
+  AS_HELP_STRING([--enable-wlink], [Enable building support for WLINK]),
+  [build_wlink=$enableval], [build_wlink=no])
 
 AC_ARG_ENABLE([jtag_dpi],
   AS_HELP_STRING([--enable-jtag_dpi], [Enable building support for JTAG DPI]),
@@ -513,7 +518,11 @@ AS_IF([test "x$build_jtag_vpi" = "xyes"], [
 ], [
   AC_DEFINE([BUILD_JTAG_VPI], [0], [0 if you don't want JTAG VPI.])
 ])
-
+AS_IF([test "x$build_wlink" = "xyes"], [
+  AC_DEFINE([BUILD_WLINK], [1], [1 if you want WLINK.])
+], [
+  AC_DEFINE([BUILD_WLINK], [0], [0 if you don't want WLINK.])
+])
 AS_IF([test "x$build_jtag_dpi" = "xyes"], [
   AC_DEFINE([BUILD_JTAG_DPI], [1], [1 if you want JTAG DPI.])
 ], [
@@ -690,6 +699,7 @@ AM_CONDITIONAL([BCM2835GPIO], [test "x$build_bcm2835gpio" = "xyes"])
 AM_CONDITIONAL([IMX_GPIO], [test "x$build_imx_gpio" = "xyes"])
 AM_CONDITIONAL([BITBANG], [test "x$build_bitbang" = "xyes"])
 AM_CONDITIONAL([JTAG_VPI], [test "x$build_jtag_vpi" = "xyes" -o "x$build_jtag_vpi" = "xyes"])
+AM_CONDITIONAL([WLINK], [test "x$build_wlink" = "xyes" -o "x$build_wlink" = "xyes"])
 AM_CONDITIONAL([JTAG_DPI], [test "x$build_jtag_dpi" = "xyes" -o "x$build_jtag_dpi" = "xyes"])
 AM_CONDITIONAL([USB_BLASTER_DRIVER], [test "x$enable_usb_blaster" != "xno" -o "x$enable_usb_blaster_2" != "xno"])
 AM_CONDITIONAL([AMTJTAGACCEL], [test "x$build_amtjtagaccel" = "xyes"])

src/flash/nor/Makefile.am
@@ -67,6 +67,8 @@ NOR_DRIVERS = \
 	%D%/stm32l4x.c \
 	%D%/stm32h7x.c \
 	%D%/gd32vf103.c \
+	%D%/wcharm.c \
+	%D%/wchriscv.c \
 	%D%/str7x.c \
 	%D%/str9x.c \
 	%D%/str9xpec.c \

src/flash/nor/drivers.c
@@ -45,6 +45,8 @@ extern const struct flash_driver fm3_flash;
 extern const struct flash_driver fm4_flash;
 extern const struct flash_driver fespi_flash;
 extern const struct flash_driver gd32vf103_flash;
+extern const struct flash_driver wch_arm_flash;
+extern const struct flash_driver wch_riscv_flash;
 extern const struct flash_driver jtagspi_flash;
 extern const struct flash_driver kinetis_flash;
 extern const struct flash_driver kinetis_ke_flash;
@@ -154,6 +156,8 @@ static const struct flash_driver * const flash_drivers[] = {
 	&stm32l4x_flash,
 	&stm32h7x_flash,
 	&gd32vf103_flash,
+	&wch_arm_flash,
+	&wch_riscv_flash,
 	&stmsmi_flash,
 	&stmqspi_flash,
 	&str7x_flash,

src/flash/nor/wchriscv.c
@@ -1,0 +1,179 @@
+/***************************************************************************
+ *   WCH RISC-V mcu :CH32V103X CH32V20X CH32V30X CH56X CH57X CH58X         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ ***************************************************************************/
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "imp.h"
+#include <helper/binarybuffer.h>
+#include <target/algorithm.h>
+extern int wlink_erase(void);
+extern unsigned char riscvchip;
+extern int wlink_reset();
+extern int wlink_write(const uint8_t *buffer, uint32_t offset, uint32_t count);
+extern int noloadflag;
+struct ch32vx_options
+{
+	uint8_t rdp;
+	uint8_t user;
+	uint16_t data;
+	uint32_t protection;
+};
+
+struct ch32vx_flash_bank
+{
+	struct ch32vx_options option_bytes;
+	int ppage_size;
+	int probed;
+
+	bool has_dual_banks;
+	bool can_load_options;
+	uint32_t register_base;
+	uint8_t default_rdp;
+	int user_data_offset;
+	int option_offset;
+	uint32_t user_bank_size;
+};
+
+FLASH_BANK_COMMAND_HANDLER(ch32vx_flash_bank_command)
+{
+	struct ch32vx_flash_bank *ch32vx_info;
+
+	if (CMD_ARGC < 6)
+		return ERROR_COMMAND_SYNTAX_ERROR;
+
+	ch32vx_info = malloc(sizeof(struct ch32vx_flash_bank));
+
+	bank->driver_priv = ch32vx_info;
+	ch32vx_info->probed = 0;
+	ch32vx_info->has_dual_banks = false;
+	ch32vx_info->can_load_options = false;
+	ch32vx_info->user_bank_size = bank->size;
+
+	return ERROR_OK;
+}
+
+static int ch32vx_erase(struct flash_bank *bank, int first, int last)
+{	
+	 
+	if(noloadflag)
+		return ERROR_OK;
+	wlink_reset();
+	int ret = wlink_erase();
+	target_halt(bank->target);
+	if (ret)
+		return ERROR_OK;
+	else
+		return ERROR_FAIL;
+}
+
+static int ch32vx_write(struct flash_bank *bank, const uint8_t *buffer,
+						uint32_t offset, uint32_t count)
+{
+	if(noloadflag)
+		return ERROR_OK;
+	int ret = wlink_write(buffer, offset, count);
+	if((riscvchip==0x02)||(riscvchip==0x03))
+		 wlink_reset(); 
+	return ret;
+}
+
+static int ch32vx_get_device_id(struct flash_bank *bank, uint32_t *device_id)
+{
+	struct target *target = bank->target;
+	int retval = target_read_u32(target, 0x1ffff7e8, device_id);
+	if (retval != ERROR_OK)
+		return retval;
+	return ERROR_OK;
+}
+
+static int ch32vx_get_flash_size(struct flash_bank *bank, uint16_t *flash_size_in_kb)
+{
+
+	struct target *target = bank->target;
+	int retval = target_read_u16(target, 0x1ffff7e0, flash_size_in_kb);
+	if (retval != ERROR_OK)
+		return retval;
+	return ERROR_OK;
+}
+
+static int ch32vx_probe(struct flash_bank *bank)
+{
+	struct ch32vx_flash_bank *ch32vx_info = bank->driver_priv;
+	uint16_t delfault_max_flash_size=512;
+	uint16_t flash_size_in_kb;
+	uint32_t device_id;
+	int page_size;
+	uint32_t base_address = 0x00000000;
+	uint32_t rid = 0;
+	ch32vx_info->probed = 0;
+	/* read ch32 device id register */
+	int retval = ch32vx_get_device_id(bank, &device_id);
+	if (retval != ERROR_OK)
+		return retval;
+
+	LOG_INFO("device id = 0x%08" PRIx32 "", device_id);
+	page_size = 1024;
+	ch32vx_info->ppage_size = 4;
+	/* get flash size from target. */
+	retval = ch32vx_get_flash_size(bank, &flash_size_in_kb);
+	if(flash_size_in_kb)
+		LOG_INFO("flash size = %dkbytes", flash_size_in_kb);
+	else
+		flash_size_in_kb=delfault_max_flash_size;
+	// /* calculate numbers of pages */
+	int num_pages = flash_size_in_kb * 1024 / page_size;
+	bank->base = base_address;
+	bank->size = (num_pages * page_size);
+	bank->num_sectors = num_pages;
+	bank->sectors = alloc_block_array(0, page_size, num_pages);
+	ch32vx_info->probed = 1;
+
+	return ERROR_OK;
+}
+
+static int ch32vx_auto_probe(struct flash_bank *bank)
+{
+
+	struct ch32vx_flash_bank *ch32vx_info = bank->driver_priv;
+	if (ch32vx_info->probed)
+		return ERROR_OK;
+	return ch32vx_probe(bank);
+}
+
+static const struct command_registration ch32vx_command_handlers[] = {
+	{
+		.name = "wch_riscv",
+		.mode = COMMAND_ANY,
+		.help = "wch_riscv flash command group",
+		.usage = "",
+
+	},
+	COMMAND_REGISTRATION_DONE};
+
+const struct flash_driver wch_riscv_flash = {
+	.name = "wch_riscv",
+	.commands = ch32vx_command_handlers,
+	.flash_bank_command = ch32vx_flash_bank_command,
+	.erase = ch32vx_erase,
+	.write = ch32vx_write,
+	.read = default_flash_read,
+	.probe = ch32vx_probe,
+	.auto_probe = ch32vx_auto_probe,
+	.erase_check = default_flash_blank_check,
+	.free_driver_priv = default_flash_free_driver_priv,
+};

src/jtag/core.c
@@ -48,7 +48,7 @@
 
 /* ipdbg are utilities to debug IP-cores. It uses JTAG for transport. */
 #include "server/ipdbg.h"
-
+extern bool wchwlink;
 /** The number of JTAG queue flushes (for profiling and debugging purposes). */
 static int jtag_flush_queue_count;
 
@@ -1227,6 +1227,10 @@ static int jtag_examine_chain(void)
 	 */
 	LOG_DEBUG("DR scan interrogation for IDCODE/BYPASS");
 	retval = jtag_examine_chain_execute(idcode_buffer, max_taps);
+	
+	if(wchwlink){
+		buf_set_u32(idcode_buffer, 0, 32, 0x00001);  //Default value,for reuse risc-v jtag debug
+	}
 	if (retval != ERROR_OK)
 		goto out;
 	if (!jtag_examine_chain_check(idcode_buffer, max_taps)) {
@@ -1404,7 +1408,7 @@ static int jtag_validate_ircapture(void)
 		 */
 		uint64_t val = buf_get_u64(ir_test, chain_pos, tap->ir_length);
 		if ((val & tap->ir_capture_mask) != tap->ir_capture_value) {
-			LOG_ERROR("%s: IR capture error; saw 0x%0*" PRIx64 " not 0x%0*" PRIx32,
+			LOG_DEBUG("%s: IR capture error; saw 0x%0*" PRIx64 " not 0x%0*" PRIx32,
 				jtag_tap_name(tap),
 				(tap->ir_length + 7) / tap->ir_length, val,
 				(tap->ir_length + 7) / tap->ir_length, tap->ir_capture_value);

src/jtag/drivers/Makefile.am
@@ -182,6 +182,10 @@ endif
 if XDS110
 DRIVERFILES += %D%/xds110.c
 endif
+if WLINK
+DRIVERFILES += %D%/wlink.c
+endif
+
 
 DRIVERHEADERS = \
 	%D%/bitbang.h \

commit 266d7c8d3f6ceae5e600de13d39a1dedc6b1d604

KARL: restore script dir searching
For.... reasons?  the MounRiver people "fixed" the search paths?  I
don't know what they were trying to do, but this breaks installations.
If you use "make install" and the bin ends up on a path that you expect,
then the scripts are in expected locations, and you don't need any "-s"
arguments.  Put it back how it was before MRS got their hands on it.

Fixes: a82b704157 Import MounRiver WCH fork
Change-Id: If48aef04659f591e0b04788d11131761126440ef
Signed-off-by: Karl Palsson <karlp@tweak.au>

src/helper/options.c
@@ -263,8 +263,7 @@ static void add_default_dirs(void)
 		free(path);
 	}
 
-	//path = alloc_printf("%s/%s/%s", exepath, bin2data, "scripts");
-	path = alloc_printf("%s/%s/%s", exepath, "../", "scripts");
+	path = alloc_printf("%s/%s/%s", exepath, bin2data, "scripts");
 	if (path) {
 		add_script_search_dir(path);
 		free(path);